faefc289531ac3312d42b5bd3e71e23a2a5fdd50
Margarita10635 <velaperezmargarita@gmail.com>
2026-06-05 15:57:20 -0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          questions: ['./src/questions.js'],
-        }
-      }
-    }
+    chunkSizeWarningLimit: 2000,
   }
 })
